1725b5a58e555a0c40dca72930ff2d455b1b65e7
Damian Ruta <damianruta@mbp-dr.local>
2019-07-25 20:54:05 +0200

    FirebaseFunctions - poprawki

--- SendEmailFbFunction/functions/index.js
+++ SendEmailFbFunction/functions/index.js
@@ -4,12 +4,6 @@ const nodemailer = require('nodemailer');
 const cors = require('cors')({ origin: true });
 admin.initializeApp();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
 
 let transporter = nodemailer.createTransport({
     service: 'gmail',
@@ -19,10 +13,20 @@ let transporter = nodemailer.createTransport({
     }
 });
 
+// $mail->isSMTP();                                      
+// $mail->Host = 'serwer1351051.home.pl';  
+// $mail->SMTPAuth = true;                               
+// $mail->Username = 'www@serwer1351051.home.pl';                 
+// $mail->Password = '&UQsJYsx+5tN';                           
+// $mail->SMTPSecure = 'ssl';                            
+// $mail->Port = 465;   
 
-let htmlMsg = `<p style="font-size: 16px;">Wiadomość testowa</p>
-    <b>Witam Cię!</b>`
+// $mail->setFrom('www@serwer1351051.home.pl', 'WWW IkanoIndustry');
+// $mail->addAddress('iod@ikanoindustry.pl', 'Rekrutacja');     
+// $mail->CharSet = 'UTF-8';
 
+
+// https://us-central1-ikanoindustyweb.cloudfunctions.net/sendMail?msg=Witaj świecie&sender=dersalis@outlook.com&ip=192.168.0.101
 exports.sendMail = functions.https.onRequest((req, res) => {
     cors(req, res, () => {
         const msg = req.query.msg;
@@ -32,10 +36,10 @@ exports.sendMail = functions.https.onRequest((req, res) => {
         const htmlMsg = createHtmlMessage(msg, sender, ip);
 
         const mailOptions = {
-            from: sender, // Something like: Jane Doe <janedoe@gmail.com>
+            from: '"WWW Ikano Industry" <www@serwer1351051.home.pl>', // Something like: Jane Doe <janedoe@gmail.com>
             to: 'dersalis@outlook.com',
-            subject: 'Wiadomość do IDO', // email subject
-            text: 'To tylko test wiadomości', // Wiadomość w formacie text
+            subject: 'Wiadomość do IOD', // email subject
+            //text: 'To tylko test wiadomości', // Wiadomość w formacie text
             html: htmlMsg,
         };
 
@@ -49,21 +53,16 @@ exports.sendMail = functions.https.onRequest((req, res) => {
     });
 });
 
+// Zwraca sformatowaną datę
 function getFormatedDate() {
     let current_datetime = new Date()
     let formatted_date = current_datetime.getFullYear() + "-" + (current_datetime.getMonth() + 1) + "-" + current_datetime.getDate() + " " + current_datetime.getHours() + ":" + current_datetime.getMinutes() + ":" + current_datetime.getSeconds()
     return formatted_date;
 }
 
+// Zwraca sformatowaną treść wiadomości
 function createHtmlMessage(message, sender, ip) {
     const currDate = new Date();
     const msg = '<span>Wiadomość do zgłaszającego </span>' + sender + ':</br></br><b>' + message + '</b></br></br><span> Wysłano: </span>' + getFormatedDate() + ' z IP: ' + ip + '</br>';
     return msg;
 }
-
-// const express = require('express');
-// const app = express();
-
-// app.get('/msg', async (req, res) => {
-//     return res.send('Wiadomość wysłana');
-// });
