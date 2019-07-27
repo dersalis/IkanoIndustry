ead02132ce7ff381fd0288806be2cf85cab93e31
dersalis <dersalis@outlook.com>
2019-07-27 14:23:09 +0200

    Tłumaczenia

--- Web/src/app/pages/information-clause/information-clause.component.ts
+++ Web/src/app/pages/information-clause/information-clause.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FacebookService, InitParams } from 'ngx-facebook';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-information-clause',
@@ -10,8 +11,18 @@ export class InformationClauseComponent implements OnInit {
 
   public message: string = "";
   public contact: string = "";
+  private ipAddress: string = "";
 
-  constructor(private fb: FacebookService) {
+  // Zwróć IP użytkownika
+  private getIP(): void {
+    this.http.get<{ip:string}>('https://jsonip.com')
+    .subscribe( data => {
+      //console.log('th data', data);
+      this.ipAddress = data.ip;
+    });
+  }
+
+  constructor(private fb: FacebookService, private http: HttpClient) {
     let initParams: InitParams = {
       // appId: '1234566778',
       xfbml: true,
@@ -19,10 +30,11 @@ export class InformationClauseComponent implements OnInit {
     };
 
     fb.init(initParams);
+
+    this.getIP();
   }
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   public sendMessage(): void {
     console.log(this.message + ' - ' + this.contact);
@@ -30,6 +42,9 @@ export class InformationClauseComponent implements OnInit {
     // Wyczyść pola
     this.message = "";
     this.contact = "";
+    console.log('IP: ' + this.ipAddress);
   }
+
+
 
 }
