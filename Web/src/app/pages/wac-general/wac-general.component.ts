9506caec8e7d3a97b32af5f3263040d6a436b433
dersalis <dersalis@outlook.com>
2019-08-13 22:08:05 +0200

    Linki

--- Web/src/app/pages/wac-general/wac-general.component.ts
+++ Web/src/app/pages/wac-general/wac-general.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FacebookService, InitParams } from 'ngx-facebook';
 
 @Component({
   selector: 'app-wac-general',
@@ -7,7 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WacGeneralComponent implements OnInit {
 
-  constructor() { }
+  constructor(private fb: FacebookService) {
+    let initParams: InitParams = {
+      // appId: '1234566778',
+      xfbml: true,
+      version: 'v2.8'
+    };
+
+    fb.init(initParams);
+  }
 
   ngOnInit() {
   }
