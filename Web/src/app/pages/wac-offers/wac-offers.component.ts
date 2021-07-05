85331f981b5050b46f149cb4e80a1e74160a23d5
Damian Ruta <damian.ruta@ikanoindustry.pl>
2021-07-05 09:38:31 +0200

    sap2021

--- Web/src/app/pages/wac-offers/wac-offers.component.ts
+++ Web/src/app/pages/wac-offers/wac-offers.component.ts
@@ -58,10 +58,10 @@ export class WacOffersComponent implements OnInit {
     // this.joboffersService.getJobOffers().subscribe(response => {
     //   this.jobOffers = response;
     // });
-    console.log('Z PHP:');
+    // console.log('Z PHP:');
     this.joboffersService.getOffersFromPhp().subscribe(
       result => {
-      console.log(result);
+      // console.log(result);
       this.jobOffersFromPhp = result;
       },
       error => {
